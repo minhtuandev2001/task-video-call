4ff8cef68b892723ac189e89e1ba43266055771b
minhtuan2001 <minhtuandev2001@gmail.com>
2024-04-19 20:28:53 +0700

    test

--- src/components/videoCall/VideoCall.js
+++ src/components/videoCall/VideoCall.js
@@ -6,6 +6,7 @@ import micTalk from "../../asset/image/micTalk.svg"
 import micOn from "../../asset/image/micOn.svg"
 import cameraOn from "../../asset/image/cameraOn.svg"
 import cameraOff from "../../asset/image/cameraOff.svg"
+import btnLeave from "../../asset/image/leave.svg"
 
 import AgoraRTC from 'agora-rtc-sdk-ng'
 import AgoraRTM from "agora-rtm-sdk"
@@ -49,19 +50,15 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
     getChannelMembers()
 
     channel.on("ChannelMessage", (message, peerId) => {
-      console.log("message :", message)
-      console.log("peerId :", peerId)
-      // handleInsertMess(message, peerId)
+      handleReceiveMessage(message, peerId)
     })
     window.addEventListener('beforeunload', leaveRtmChannel)
     channel.on('MemberJoined', handleMemberJoined)
     channel.on('MemberLeft', handleMemberLeft)
   }
 
-
   const initRtc = async () => {
     rtcClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" }) // create client rtc
-
     // event
     // rtcClient.on('user-joined', handleUserJoined)
     rtcClient.on("user-published", handleUserPublished)
@@ -90,13 +87,9 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
         <div class="video-display" id="stream-${uid}">
         </div>
         <div class="video-desc">
-            <div class="">
-            <button>1</button>
-            </div>
             <p>${uid} owner </p>
             </div>
             </div>`
-          // <button class="user-rtc-${uid}" id="button-${uid}" ><img id="${uid}" src=${micOff} alt="" /></button>
           videosRef.current.insertAdjacentHTML('afterbegin', userVideo)
           localTracks.videoTrack.play(`stream-${uid}`)
           await rtcClient.publish([localTracks.videoTrack])
@@ -108,13 +101,9 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
           let userVideo = `<div class="video-wrapper" id="user-${uid}">
           <img class='avatar' src="https://images.unsplash.com/photo-1712569490441-0c7cc00e6768?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
           <div class="video-desc">
-            <div class="">
-            <button>1</button>
-            </div>
             <p>${uid} owner ko camera</p>
             </div>
             </div>`
-          // <button class="user-rtc-${uid}" id="button-${uid}" ><img id="${uid}" src=${micOff} alt="" /></button>
           videosRef.current.insertAdjacentHTML('afterbegin', userVideo)
         });
     }
@@ -125,27 +114,65 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
   }
 
   function handleToogleMicMemeber(id) {
-    console.log("check e", id)
+    let content = {
+      type: "action-mic",
+      id: id
+    }
+    channel.sendMessage({ text: JSON.stringify(content), type: "text" })
+    console.log("check mic", id)
   }
   function handleToogleCameraMemeber(id) {
-    console.log("check e", id)
+    // channel.sendMessage({ text: "hello mn", type: "text" })
+    console.log("check camera", id)
+  }
+  function handleToogleLeaveMemeber(id) {
+    // channel.sendMessage({ text: "hello mn", type: "text" })
+    console.log("check leave", id)
+  }
+  const handleReceiveMessage = (msg, Peerid) => {
+    let content = JSON.parse(msg.text);
+    alert(`${content.id} : ${Peerid}`)
+    if (content.id === uid) {
+      switch (content.type) {
+        case 'action-mic':
+          if (micCanUse.current) {
+            toggleMic()
+          } else {
+            let content = {
+              type: "send-error",
+              id: Peerid
+            }
+            channel.sendMessage({ text: JSON.stringify(content), type: "text" })
+          }
+          break;
+        case 'send-error':
+          // nơi chủ phòng nhận lại lỗi của các member gặp phải
+          alert(`loi`)
+          break;
+
+        default:
+          break;
+      }
+    }
   }
   let handleMemberJoined = async (MemberId) => {
     const { name, userRtcUid, avatar } = await rtmClient.getUserAttributesByKeys(MemberId, ['name', 'userRtcUid', 'avatar'])
     let userVideo = `<div class="video-wrapper" id="user-${MemberId}">
           <img class='avatar' src=${avatar} alt="" />
           <div class="video-desc">
-            <div class="">
+            <div class="video">
             <button class="user-rtc-${MemberId}" id="button-${MemberId}" ><img src=${micOff} alt="" /></button>
             <button class="user-rtc-camera-${MemberId}" id="button-camera-${MemberId}" ><img src=${cameraOff} alt="" /></button>
+            <button class="user-rtc-leave-${MemberId}" id="button-leave-${MemberId}" ><img src=${btnLeave} alt="" /></button>
             </div>
-            <p>${MemberId} tham gia</p> 
+            <p>${name} tham gia</p> 
           </div>
           </div>`
     // check chủ phòng afterbegin
     videosRef.current.insertAdjacentHTML('beforeend', userVideo)
     let buttonMicUser = videosRef.current.querySelector(`#button-${MemberId}`);
     let buttonCamUser = videosRef.current.querySelector(`#button-camera-${MemberId}`);
+    let buttonLeaveUser = videosRef.current.querySelector(`#button-leave-${MemberId}`);
     if (buttonMicUser) {
       buttonMicUser.addEventListener('click', (e) => {
         handleToogleMicMemeber(MemberId)
@@ -154,6 +181,11 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
     if (buttonCamUser) {
       buttonCamUser.addEventListener('click', (e) => {
         handleToogleCameraMemeber(MemberId)
+      })
+    }
+    if (buttonLeaveUser) {
+      buttonLeaveUser.addEventListener('click', (e) => {
+        handleToogleLeaveMemeber(MemberId)
       })
     }
   }
@@ -165,47 +197,37 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
         let newMember = `<div class="video-wrapper" id="user-${members[i]}">
         <img class='avatar' src=${avatar} alt="" />
         <div class="video-desc">
-          <div class="">
+          <div class="video-action">
           <button class="user-rtc-${members[i]}" id="button-${members[i]}" ><img src=${micOff} alt="" /></button>
           <button class="user-rtc-camera-${members[i]}" id="button-camera-${members[i]}" ><img src=${cameraOff} alt="" /></button>
+          <button class="user-rtc-leave-${members[i]}" id="button-leave-${members[i]}" ><img src=${btnLeave} alt="" /></button>
           </div>
-          <p>${members[i]} tham gia</p> 
+          <p>${name} tham gia</p> 
         </div>
         </div>`
         videosRef.current.insertAdjacentHTML('beforeend', newMember)
+        let buttonMicUser = videosRef.current.querySelector(`#button-${members[i]}`);
+        let buttonCamUser = videosRef.current.querySelector(`#button-camera-${members[i]}`);
+        let buttonLeaveUser = videosRef.current.querySelector(`#button-leave-${members[i]}`);
+        if (buttonMicUser) {
+          buttonMicUser.addEventListener('click', (e) => {
+            handleToogleMicMemeber(members[i])
+          })
+        }
+        if (buttonCamUser) {
+          buttonCamUser.addEventListener('click', (e) => {
+            handleToogleCameraMemeber(members[i])
+          })
+        }
+        if (buttonLeaveUser) {
+          buttonLeaveUser.addEventListener('click', (e) => {
+            handleToogleLeaveMemeber(members[i])
+          })
+        }
+
       }
     }
   }
-  // const handleUserJoined = async (user) => {
-  //   console.log("check 1", name)
-  //   console.log("check 1", userRtcUid)
-  //   console.log("check 1", avatar)
-  //   // trường hợp giành cho 2 trình duyệt khác nhau dùng 1 camera 
-  //   let userVideo = `<div class="video-wrapper" id="user-${user.uid}">
-  //         <img class='avatar' src=${avatar} alt="" />
-  //         <div class="video-desc">
-  //           <div class="">
-  //           <button class="user-rtc-${user.uid}" id="button-${user.uid}" ><img src=${micOff} alt="" /></button>
-  //           <button class="user-rtc-camera-${user.uid}" id="button-camera-${user.uid}" ><img src=${cameraOff} alt="" /></button>
-  //           </div>
-  //           <p>${user.uid} tham gia</p> 
-  //         </div>
-  //         </div>`
-  //   // check chủ phòng afterbegin
-  //   videosRef.current.insertAdjacentHTML('beforeend', userVideo)
-  //   let buttonMicUser = videosRef.current.querySelector(`#button-${user.uid}`);
-  //   let buttonCamUser = videosRef.current.querySelector(`#button-camera-${user.uid}`);
-  //   if (buttonMicUser) {
-  //     buttonMicUser.addEventListener('click', (e) => {
-  //       handleToogleMicMemeber(user.uid)
-  //     })
-  //   }
-  //   if (buttonCamUser) {
-  //     buttonCamUser.addEventListener('click', (e) => {
-  //       handleToogleCameraMemeber(user.uid)
-  //     })
-  //   }
-  // }
   const handleUserPublished = async (user, mediaType) => {
     console.log("check 2")
     await rtcClient.subscribe(user, mediaType)
@@ -217,9 +239,10 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
         userElement.innerHTML = `<div class="video-display" id="stream-${user.uid}">
         </div>
         <div class="video-desc">
-            <div class="">
+            <div class="video-action">
             <button class="user-rtc-${user.uid}" id="button-${user.uid}" ><img src=${micOff} alt="" /></button>
             <button class="user-rtc-camera-${user.uid}" id="button-camera-${user.uid}" ><img src=${cameraOn} alt="" /></button>
+            <button class="user-rtc-leave-${user.uid}" id="button-leave-${user.uid}" ><img src=${btnLeave} alt="" /></button>
             </div>
             <p>${user.uid} client co camera</p>
           </div>`
@@ -228,9 +251,10 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
         <div class="video-display" id="stream-${user.uid}">
         </div>
         <div class="video-desc">
-            <div class="">
+            <div class="video-action">
             <button class="user-rtc-${user.uid}" id="button-${user.uid}" ><img src=${micOff} alt="" /></button>
             <button class="user-rtc-camera-${user.uid}" id="button-camera-${user.uid}" ><img src=${cameraOn} alt="" /></button>
+            <button class="user-rtc-leave-${user.uid}" id="button-leave-${user.uid}" ><img src=${btnLeave} alt="" /></button>
             </div>
             <p>${user.uid} owner </p>
           </div>
@@ -241,6 +265,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       }
       let buttonMicUser = videosRef.current.querySelector(`#button-${user.uid}`);
       let buttonCamUser = videosRef.current.querySelector(`#button-camera-${user.uid}`);
+      let buttonLeaveUser = videosRef.current.querySelector(`#button-leave-${user.uid}`);
       if (buttonMicUser) {
         buttonMicUser.addEventListener('click', (e) => {
           handleToogleMicMemeber(user.uid)
@@ -251,12 +276,25 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
           handleToogleCameraMemeber(user.uid)
         })
       }
+      if (buttonLeaveUser) {
+        buttonLeaveUser.addEventListener('click', (e) => {
+          handleToogleLeaveMemeber(user.uid)
+        })
+      }
       user.videoTrack.play(`stream-${user.uid}`)
     }
     if (mediaType === 'audio') {
+      let buttonMicUser = videosRef.current.querySelector(`#button-${user.uid}`);
+      buttonMicUser.innerHTML = `<img src=${micOn} alt="" />`
+      if (buttonMicUser) {
+        buttonMicUser.addEventListener('click', (e) => {
+          handleToogleMicMemeber(user.uid) // mở mic member
+        })
+      }
       user._audioTrack.play();
     }
   }
+
   const handleUserUnPublished = async (user, mediaType) => {
     const { name, userRtcUid, avatar } = await rtmClient.getUserAttributesByKeys(user.uid, ['name', 'userRtcUid', 'avatar'])
     if (mediaType === 'video') {
@@ -264,11 +302,12 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       if (userElement) {
         userElement.innerHTML = `<img class='avatar' src=${avatar} alt="" />
         <div class="video-desc">
-          <div class="">
+          <div class="video-action">
           <button class="user-rtc-${user.uid}" id="button-${user.uid}" ><img src=${micOff} alt="" /></button>
           <button class="user-rtc-camera-${user.uid}" id="button-camera-${user.uid}" ><img src=${cameraOff} alt="" /></button>
+          <button class="user-rtc-leave-${user.uid}" id="button-leave-${user.uid}" ><img src=${btnLeave} alt="" /></button>
           </div>
-          <p>${user.uid} đã tắt camera</p>
+          <p>${name} đã tắt camera</p>
           </div>
           </div>`
       }
@@ -280,10 +319,16 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       }
     }
   }
+
   let handleUserLeft = async (user) => {
     console.log("check 1 2 3")
     delete remoteTracks[user.uid];
+    let buttonMicUser = videosRef.current.querySelector(`#button-${user.uid}`);
+    buttonMicUser.removeEventListener('click', (e) => {
+      handleToogleMicMemeber(user.uid) // mở mic member
+    })
   }
+
   let handleMemberLeft = async (MemberId) => {
     const userElement = videosRef.current.querySelector(`#user-${MemberId}`);
     if (userElement) {
@@ -296,6 +341,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
     await channel.leave();
     await rtmClient.logout();
   }
+
   const leaveRoom = async () => {
     const userElement = videosRef.current.querySelector(`#user-${uid}`);
     if (userElement) {
@@ -334,6 +380,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       })
     })
   }
+
   const toggleMic = async () => {
     // mount component thì sẽ lấy data phòng , trong đó có cho mở mic ko 
     if (micCanUse.current) {
@@ -349,6 +396,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       alert("mic khong su dung duoc")
     }
   }
+
   const toggleCamera = async (e) => {
     // mount component thì sẽ lấy data phòng , trong đó có cho mở camera ko 
     if (cameraCanUse.current) {
@@ -361,6 +409,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       alert("Camera khong su dung duoc")
     }
   }
+
   const stopVideo = async () => {
     // nhớ lấy avatar của tài khoản này thay vào 
     if (cameraActive.current === true) {
@@ -368,9 +417,6 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       if (userElement) {
         userElement.innerHTML = `<img class='avatar' src="https://images.unsplash.com/photo-1712569490441-0c7cc00e6768?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div class="video-desc">
-          <div class="">
-          <button>1</button>
-          </div>
           <p>${uid} owner tắt camera</p>
           </div>
           </div>`
@@ -379,6 +425,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       cameraActive.current = false;
     }
   }
+
   const startVideo = async () => {
     if (cameraActive.current === false) {
       const userElement = videosRef.current.querySelector(`#user-${uid}`);
@@ -386,9 +433,6 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
         userElement.innerHTML = `<div class="video-display" id="stream-${uid}">
         </div>
         <div class="video-desc">
-            <div class="">
-            <button>1</button>
-            </div>
             <p>${uid} owner </p>
             </div>
             </div>`
@@ -398,6 +442,7 @@ export default function VideoCall({ roomId, uid }) { // trong này cần id củ
       cameraActive.current = true;
     }
   }
+
   useEffect(() => {
     enterRoom()
     return () => {
